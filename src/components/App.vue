f95f67d23e4a4f70e64acd59b1fcf3fe9fd1cd94
Erik Isidore <ericssonico@protonmail.ch>
2018-03-17 11:55:18 -0300

    Add main application Vue components

---
+++ src/components/App.vue
@@ -0,0 +1,185 @@
+<script>
+import AppFooter from './AppFooter';
+
+import axios from 'axios';
+import CPF from 'gerador-validador-cpf';
+
+const usersURL = 'https://convenia-front-end-test.firebaseio.com/users.json';
+
+export default {
+  name: 'app',
+  components: { AppFooter },
+  data () {
+    return {
+      isCPFValid: false,
+      userData: { email: '', password: '' }
+    }
+  },
+  computed: {
+    isUserDataValid: function() {
+      return (this.userData.email && this.userData.password);
+    }
+  },
+  methods: {
+    validateCPF: function(ev) {
+      let value = ev.target.value.split(/\D/).join('');
+
+      // Check if user isn't just pressing backspace (ev.data !== null),
+      // then immediately remove any non-digit characters inserted by
+      // the user and return the function.
+      if (ev.data !== null && (/\D+/.test(ev.data) || value.length > 11)) {
+        ev.target.value = ev.target.value.slice(0, -1);
+        return;
+      }
+
+      // Properly format the string into the standard CPF format
+      if (value.length > 9) {
+        let cpfPrefix = value.substr(0, 9).replace(/(\d{3})(?=\d+)/g, "$1.");
+        let cpfSuffix = value.substr(9);
+        ev.target.value = `${cpfPrefix}-${cpfSuffix}`;
+      } else {
+        ev.target.value = value.replace(/(\d{3})(?=\d+)/g, "$1.");
+      }
+
+      // Check if the whole string is a valid CPF number.
+      this.isCPFValid = CPF.validate(value);
+    },
+    submitUser: async function(ev) {
+      ev.preventDefault();
+
+      try {
+        // Send data to the server then reset local user data after
+        // a successful response
+        await axios.post(usersURL, this.userData);
+        this.userData = { email: '', password: '' };
+      } catch (err) {
+        console.error("Users API Error: ", err);
+      }
+    }
+  }
+}
+</script>
+
+<template lang="html">
+  <div class="page-container d-flex flex-column">
+    <div class="container-fluid d-flex flex-column">
+      <div class="row">
+        <div class="col-md d-flex align-items-end order-md-1 right-column">
+
+          <!-- Logo wrapper -->
+          <div class="ml-3 mb-4 mt-4 ml-md-5 mb-md-5 mt-md-0">
+            <img src="~img/convenia-logo.png" alt="Convenia Logo" class="convenia-logo img-fluid ml-md-2">
+          </div>
+
+        </div>
+        <div class="col-md left-column">
+
+          <!-- left-column contents -->
+          <div class="row align-items-center h-100">
+            <div class="col mt-5 mb-5 ml-4 mr-4 mr-lg-0">
+
+              <!-- Form legend -->
+              <div class="col-xl-6 col-lg-10">
+                <div class="mb-5">
+                  <p class="h5 mb-4 text-antialiased">Olá,</p>
+                  <p class="h5 text-antialiased">
+                    Você está acessando o
+                    <a href="https://www.convenia.com.br/" target="_blank">Convenia</a>
+                    que oferece descontos e vantagens em mais de 5.000
+                    estabelecimentos. Economize na compra de produtos e
+                    serviços de marcas que interessam.
+                  </p>
+                </div>
+              </div>
+
+              <!-- Login form -->
+              <div class="col">
+                <form @submit="submitUser" class="pt-4 no-gutters">
+                  <div class="form-group input-group col-md-12 col-lg-6">
+                    <input @input="validateCPF" type="text" class="form-control" placeholder="ativar CPF">
+                    <div class="input-group-append">
+                      <button :disabled="!isCPFValid" type="button" class="btn btn-primary">
+                        Ok
+                      </button>
+                    </div>
+                  </div>
+                  <div class="form-row">
+                    <div class="col-12">
+                      <small class="form-text font-weight-bold mb-2">
+                        Se você já possui cadastro, <a href="#">faça o login</a>
+                      </small>
+                    </div>
+                    <div class="col-lg-3 mb-3 mb-lg-0">
+                      <input v-model="userData.email" type="email" class="form-control" placeholder="e-mail">
+                    </div>
+                    <div class="col-lg-3 mb-3 mb-lg-0">
+                      <input v-model="userData.password" type="password" class="form-control" placeholder="senha">
+                    </div>
+                    <div class="col-auto d-flex align-items-center">
+                      <button :disabled="!isUserDataValid" type="submit" class="btn btn-primary btn-round">
+                        Entrar
+                      </button>
+                    </div>
+                    <div class="col-12 mt-2">
+                      <small class="font-weight-bold">
+                        <a href="#">Esqueci a senha</a>
+                      </small>
+                    </div>
+                  </div>
+                </form>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <app-footer />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '~styles/variables';
+@import '~bootstrap/scss/mixins';
+
+//  Variables
+//  ---------
+
+$right-column-bg: #868686;
+
+//  Page Layout
+//  -----------
+
+.page-container {
+  min-height: 100%;
+
+  > .container-fluid {
+    flex-grow: 1;
+    > .row { flex-grow: 1; }
+  }
+}
+
+.right-column {
+  background-color: $right-column-bg;
+
+  flex: 0 0 46.998535871%;
+  max-width: 46.998535871%;
+
+  @include media-breakpoint-down(sm) {
+    flex: auto;
+    max-width: initial;
+
+    .convenia-logo { width: 8.75rem; }
+  }
+}
+
+.left-column {
+  @include media-breakpoint-down(sm) {
+    display: flex;
+    flex-direction: column;
+  }
+}
+
+</style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,138 @@
+<script>
+import axios from 'axios';
+import { Carousel, Slide } from 'vue-carousel';
+
+const partnersURL = 'https://convenia-front-end-test.firebaseio.com/partners.json';
+
+export default {
+  name: 'app-footer',
+  data: function () {
+    return { partnerList: [ ] }
+  },
+  created: function() {
+    this.getPartners();
+  },
+  methods: {
+    getPartners: async function() {
+      try {
+        const response = await axios.get(partnersURL);
+        this.partnerList = response.data;
+      } catch (err) {
+        console.error("Partners API Error: ", err);
+      }
+    }
+  },
+  components: {
+    Carousel,
+    Slide
+  }
+}
+</script>
+
+<template lang="html">
+  <footer class="app-footer">
+    <div class="container-fluid h-100">
+      <div class="row align-items-center justify-content-between h-100">
+        <div class="wrapper-partners col-12 col-md pb-2 ml-md-4 pl-md-4 pb-md-0">
+          <span class="partners-text text-muted text-antialiased">
+            Parceiros
+          </span>
+          <div class="partners col-lg-8">
+
+            <carousel
+              :navigationEnabled="true"
+              :paginationEnabled="false"
+              :perPageCustom="[[0, 3], [992, 6], [1650, 9]]"
+              navigationPrevLabel='<i class="fa fa-angle-left" />'
+              navigationNextLabel='<i class="fa fa-angle-right" />'>
+              <slide v-for="(partner, index) in partnerList" :key="index">
+                <img :src="partner.image" :alt="partner.name" class="partner-img">
+              </slide>
+            </carousel>
+
+          </div>
+        </div>
+        <div class="col-12 col-md-auto d-flex align-items-center justify-content-center mr-md-1">
+          <small class="brand-legend text-muted mr-3">desennvolvido por</small>
+          <img src="~img/convenia-brand.png" alt="Desennvolvido por Convenia" class="convenia-brand">
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss">
+@import '~styles/variables';
+@import '~bootstrap/scss/mixins';
+
+//  Variables
+//  ---------
+
+$app-footer-bg: #F3F3F4;
+$app-footer-height: 78px;
+
+$carousel-nav-width: 24px;
+$carousel-nav-height: 24px;
+
+//  Classes
+//  -------
+
+.app-footer {
+  height: $app-footer-height;
+  background-color: $app-footer-bg;
+
+  @include media-breakpoint-down(sm) {
+    height: auto;
+    padding: 15px 5px;
+    min-height: $app-footer-height;
+  }
+
+  .wrapper-partners {
+    display: flex;
+    align-items: center;
+
+    .partners-text {
+      font-size: 1rem;
+      @include media-breakpoint-down(sm) { font-size: .875rem }
+    }
+
+    .partners {
+      display: flex;
+      padding: 0 40px;
+
+      .partner-img {
+        max-width: 100%;
+        width: 65px;
+        @include media-breakpoint-down(sm) { width: 35px; }
+      }
+
+      .VueCarousel {
+        flex-basis: 100%;
+        flex-shrink: 1;
+
+        .VueCarousel-wrapper { padding: 0 5px; }
+
+        .VueCarousel-navigation .VueCarousel-navigation-button {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+          width: 24px;
+          height: 24px;
+
+          border-radius: 50%;
+          background-color: #D4D2D2;
+          color: #FFFFFF;
+
+          .fa { font-size: 1.25rem; }
+        }
+      }
+    }
+  }
+
+  .convenia-brand {
+    @include media-breakpoint-down(sm) { width: 80px; }
+  }
+}
+
+</style>
